0fd6787f4844ce5cc50a3ea186ea44a3c02edc87
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2023-06-28 21:27:35 +0800

    build: Configure shadow install

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,48 @@
+import java.io.ByteArrayOutputStream
+import java.io.FileInputStream
+import java.time.format.DateTimeFormatter
+import java.time.ZonedDateTime
+import java.time.ZoneId
+import java.util.Properties
+
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.util.distsDirectory
 
 val ktorVersion: String by project
 val cliktVersion: String by project
 val hopliteVersion: String by project
 val slf4jVersion: String by project
+
+val versionPropertiesFile = "${projectDir}/version.properties"
+
+fun String.runCommand(currentWorkingDir: File = file("./")): String {
+    val byteOut = ByteArrayOutputStream()
+    project.exec {
+        workingDir = currentWorkingDir
+        commandLine = this@runCommand.split("\\s".toRegex())
+        standardOutput = byteOut
+    }
+    return String(byteOut.toByteArray()).trim()
+}
+
+fun getRevision(): String {
+    return "git rev-parse --short=7 HEAD".runCommand()
+}
+
+fun getProperties(file: String, key: String): String {
+    val fileInputStream = FileInputStream(file)
+    val props = Properties()
+    props.load(fileInputStream)
+    return props.getProperty(key)
+}
+
+fun getVersion(): String {
+    return getProperties(versionPropertiesFile, "version")
+}
+
+fun getStage(): String {
+    return getProperties(versionPropertiesFile, "stage")
+}
 
 plugins {
     kotlin("jvm") version "1.8.21"
@@ -12,7 +51,44 @@ plugins {
 }
 
 group = "tech.ixor"
-version = "0.0.1-alpha"
+version = getVersion() + "-" + getStage() + "+" + getRevision()
+
+tasks {
+    compileKotlin {
+        kotlinOptions {
+            jvmTarget = "17"
+        }
+    }
+
+    val versionProperties by registering(WriteProperties::class) {
+        destinationFile.set { file("${buildDir}/resources/main/version.properties") }
+        encoding = "UTF-8"
+        property("version", getVersion())
+        property("stage", getStage())
+        property("revision", getRevision())
+        property("buildDate",
+            ZonedDateTime
+                .now(ZoneId.of("UTC"))
+                .format(DateTimeFormatter.ofPattern("E, MMM dd yyyy"))
+        )
+    }
+
+    var shadowJarVersion = getVersion()
+    shadowJar {
+        if (getStage() == "dev" || getStage() == "alpha" || getStage() == "beta" || getStage() == "rc") {
+            shadowJarVersion = shadowJarVersion + "-" + getStage()
+        }
+        shadowJarVersion = shadowJarVersion + "+" + getRevision()
+        archiveVersion.set(shadowJarVersion)
+        archiveClassifier.set("")
+    }
+
+    processResources {
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+        exclude("conf/config.yaml")
+        from(versionProperties)
+    }
+}
 
 repositories {
     mavenCentral()
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,3 @@
 
-rootProject.name = "LCTT-ToolKit"
+rootProject.name = "LCTT.ToolKit"
 
