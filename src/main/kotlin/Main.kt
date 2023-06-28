2e57eb69a588f13905cf57b6f01c3a38abf0b673
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2023-06-28 18:55:21 +0800

    fix: Switch to Java HTTP Client

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,15 @@
 import io.ktor.client.*
-import io.ktor.client.engine.cio.*
+import io.ktor.client.engine.*
+import io.ktor.client.engine.java.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 
 suspend fun main() {
-    val client = HttpClient(CIO)
+    val client = HttpClient(Java) {
+        engine {
+            protocolVersion = java.net.http.HttpClient.Version.HTTP_2
+        }
+    }
     val response: HttpResponse = client.get("https://ktor.io/")
     println(response.status)
     client.close()
