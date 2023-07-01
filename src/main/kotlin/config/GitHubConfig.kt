c1a253a567dbf22c152349a231d264dc12928417
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2023-07-01 13:31:02 +0800

    refactor: Change local repo path config to repo name

--- src/main/kotlin/config/GitHubConfig.kt
+++ src/main/kotlin/config/GitHubConfig.kt
@@ -5,5 +5,5 @@ import com.sksamuel.hoplite.ConfigAlias
 data class GitHubConfig (
     @ConfigAlias("github_id") val githubID: String,
     @ConfigAlias("github_token") val githubToken: String,
-    @ConfigAlias("repo_path") val repoPath: String
+    @ConfigAlias("repo_name") val repoName: String
 )
