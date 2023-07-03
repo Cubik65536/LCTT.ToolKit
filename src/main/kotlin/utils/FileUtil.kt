ba7faa24d2da6075837f9039f318a408db6d9fc6
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2023-07-03 13:45:02 +0800

    feat: Add util for unzip

--- src/main/kotlin/utils/FileUtil.kt
+++ src/main/kotlin/utils/FileUtil.kt
@@ -7,10 +7,49 @@ import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import kotlinx.coroutines.runBlocking
 import org.slf4j.LoggerFactory
+import java.io.BufferedOutputStream
 import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
+import java.util.zip.ZipFile
+
+private const val BUFFER_SIZE = 4096
 
 object FileUtil {
     private val logger = LoggerFactory.getLogger(javaClass)
+
+    // credit for unzip code: https://prakashnitin.medium.com/unzipping-files-in-android-kotlin-2a2a2d5eb7ae
+    private fun extractFile(inputStream: InputStream, destFilePath: String) {
+        val bos = BufferedOutputStream(FileOutputStream(destFilePath))
+        val bytesIn = ByteArray(BUFFER_SIZE)
+        var read: Int
+        while (inputStream.read(bytesIn).also { read = it } != -1) {
+            bos.write(bytesIn, 0, read)
+        }
+        bos.close()
+    }
+
+    fun unzip(zipFilePath: File, destDirectory: String) {
+        File(destDirectory).run {
+            if (!exists()) {
+                mkdirs()
+            }
+        }
+
+        ZipFile(zipFilePath).use { zip ->
+            zip.entries().asSequence().forEach { entry ->
+                zip.getInputStream(entry).use { input ->
+                    val filePath = destDirectory + File.separator + entry.name
+                    if (!entry.isDirectory) {
+                        extractFile(input, filePath)
+                    } else {
+                        val dir = File(filePath)
+                        dir.mkdir()
+                    }
+                }
+            }
+        }
+    }
 
     fun downloadFile(
         urlString: String,
