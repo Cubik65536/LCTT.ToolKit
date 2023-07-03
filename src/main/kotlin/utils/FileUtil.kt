84d8ed394217919ece57b2d079ff42ccb1d5c3c8
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2023-07-03 13:32:09 +0800

    style: Optimize imports

--- src/main/kotlin/utils/FileUtil.kt
+++ src/main/kotlin/utils/FileUtil.kt
@@ -1,9 +1,8 @@
 package utils
 
 import io.ktor.client.*
-import io.ktor.client.call.body
+import io.ktor.client.call.*
 import io.ktor.client.engine.java.*
-import io.ktor.client.plugins.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import kotlinx.coroutines.runBlocking
