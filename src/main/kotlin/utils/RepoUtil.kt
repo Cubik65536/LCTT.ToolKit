0d3abaf040536cff17ce6f8a43de0cb6932030e3
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2023-07-03 13:58:35 +0800

    feat: Add util for sync repo

--- src/main/kotlin/utils/RepoUtil.kt
+++ src/main/kotlin/utils/RepoUtil.kt
@@ -1,10 +1,43 @@
 package utils
 
+import com.beust.klaxon.Klaxon
 import config.GitHubConfig
 import io.ktor.client.request.*
 import io.ktor.http.*
+import org.slf4j.LoggerFactory
+
+const val GITHUB_API_URL = "https://api.github.com"
 
 class RepoUtil (private val config: GitHubConfig) {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    fun syncUpstream() {
+        logger.info("Syncing upstream...")
+
+        val url = "${GITHUB_API_URL}/repos/${config.githubID}/${config.repoName}/erge-upstream"
+        val response = HttpUtil.PostRequests.request(url) {
+            accept(ContentType("application", "vnd.github+json"))
+            bearerAuth(config.githubToken)
+            headers {
+                append("X-GitHub-Api-Version", "2022-11-28")
+            }
+            contentType(ContentType.Application.Json)
+            setBody(
+                Klaxon().toJsonString(
+                    mapOf(
+                        "branch" to "master"
+                    )
+                )
+            )
+        }
+
+        when (response.status.value) {
+            200 -> logger.info("Successfully synced with the upstream repository.")
+            409 -> logger.error("Could not be sync with upstream repository because of a merge conflict.")
+            else -> logger.error("Could not be sync with upstream repository for unknown reason.")
+        }
+    }
+
     fun downloadArchive() {
         val url = "https://github.com/${config.githubID}/${config.repoName}/zipball/master"
         val path = "tmp/${config.repoName}.zip"
